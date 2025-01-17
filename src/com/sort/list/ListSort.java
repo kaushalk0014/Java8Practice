b51dece7cdb573cb7b35ca4f04bc33fd7afa4fa1
kaushal kumar <kaushal.k.0014@gmail.com>
2025-01-17 08:54:46 +0530

    sort primitive data

--- src/com/sort/list/ListSort.java
+++ src/com/sort/list/ListSort.java
@@ -1,22 +1,40 @@
 package com.sort.list;
 
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 
 public class ListSort {
 
 	public static void main(String[] args) {
-		List<Integer> list=getIntValue();
+		List<Integer> list=getListValue();
 		list.sort(Comparator.reverseOrder());// descending order  
+		System.out.println("Descending order sort : "+list);
 		
-		System.out.println(list);
+		List<Integer> intList=getListValue();
+		intList.sort(Comparator.naturalOrder());
+		System.out.println("Ascending order sort : "+intList);
 		
+		
+		System.out.println("\n ------Using Collections class------");
+		List<Integer> conList=getListValue();
+		Collections.sort(conList); //Ascending order
+		
+		System.out.println("Ascending order sort : "+conList);
+		
+		//or we can use Comparator.naturalOrder to sort data in ascending order
+		Collections.sort(conList, Comparator.reverseOrder());
+		System.out.println("Comparator.reverseOrder() : "+ conList);
+		
+		Collections.sort(conList, Comparator.naturalOrder());
+		
+		System.out.println("Comparator.naturalOrder() : "+conList);
 		
 	}
 	
 	
-	private static List<Integer> getIntValue(){
+	private static List<Integer> getListValue(){
 		return Arrays.asList(10,8,6,9,16,13,18,17);
 	}
 }
