ccf27496029f5b225711cc375f700adfa8d20bda
kaushal kumar <kaushal.k.0014@gmail.com>
2025-02-07 21:42:12 +0530

    adding Nth highest salary using map

--- src/com/example/nth/salary/NthHighestSalaryExample.java
+++ src/com/example/nth/salary/NthHighestSalaryExample.java
@@ -1,25 +1,55 @@
 package com.example.nth.salary;
 
+import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.Map.Entry;
 import java.util.stream.Collectors;
 
 public class NthHighestSalaryExample {
 
 	public static void main(String[] args) {
-		Map<String, Integer> map = new HashMap<String, Integer>();
-		map.put("Pankaj", 2500);
-		map.put("Rakesh", 1600);
-		map.put("Anil", 1500);
-		map.put("Dinesh", 1400);
-		map.put("Rajesh", 440);
-		
-		Map.Entry<String, Integer> resultData= map.entrySet().stream()
-				.sorted(Collections.reverseOrder(Map.Entry.comparingByValue()))
-			.collect(Collectors.toList())
-			.get(1);
-		
+
+		Map.Entry<String, Integer> resultData = getMap().entrySet().stream()
+				.sorted(Collections.reverseOrder(Map.Entry.comparingByValue())).collect(Collectors.toList()).get(1);
+
 		System.out.println(resultData);
+
+		System.out.println("Approach 2: " + nthHighestSalary(2, getMap()));
+		System.out.println("Approach 3: " + nthHighestSalary3(2, getMap()));
+	}
+
+	public static Entry<String, Integer> nthHighestSalary(int index, Map<String, Integer> map) {
+		return map.entrySet().stream().sorted(Collections.reverseOrder(Map.Entry.comparingByValue()))
+				.collect(Collectors.toList()).get(index-1);
+
+	}
+
+	public static Entry<String, Integer> nthHighestSalary3(int index, Map<String, Integer> map) {
+
+		List<Entry<String, Integer>> list = new ArrayList<Map.Entry<String, Integer>>(map.entrySet());
+
+		Collections.sort(list, new Comparator<Entry<String, Integer>>() {
+			@Override
+			public int compare(Entry<String, Integer> o1, Entry<String, Integer> o2) {
+				return o2.getValue()-o1.getValue();
+			}
+		});
+		return list.get(index-1);
+
+	}
+
+	public static Map<String, Integer> getMap() {
+		Map<String, Integer> map = new HashMap<String, Integer>();
+		map.put("Pankaj", 1500);
+		map.put("Rakesh", 1600);
+		map.put("Anil", 1200);
+		map.put("Dinesh", 1400);
+		map.put("Rajesh", 1700);
+
+		return map;
 	}
 }
