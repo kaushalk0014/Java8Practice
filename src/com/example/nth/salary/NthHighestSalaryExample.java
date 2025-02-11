f4cc675f73abc844d9d92cecafa3361ee3b8e7d4
kaushal kumar <kaushal.k.0014@gmail.com>
2025-02-11 21:40:42 +0530

    adding nth highest salary code

--- src/com/example/nth/salary/NthHighestSalaryExample.java
+++ src/com/example/nth/salary/NthHighestSalaryExample.java
@@ -8,6 +8,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class NthHighestSalaryExample {
 
@@ -20,9 +21,11 @@ public class NthHighestSalaryExample {
 
 		System.out.println("Approach 2: " + nthHighestSalary(2, getMap()));
 		System.out.println("Approach 3: " + nthHighestSalary3(2, getMap()));
+		System.out.println("Approach 4: " + nthHighestSalary4(1, getMap2()));
 	}
 
 	public static Entry<String, Integer> nthHighestSalary(int index, Map<String, Integer> map) {
+		
 		return map.entrySet().stream().sorted(Collections.reverseOrder(Map.Entry.comparingByValue()))
 				.collect(Collectors.toList()).get(index-1);
 
@@ -41,6 +44,17 @@ public class NthHighestSalaryExample {
 		return list.get(index-1);
 
 	}
+	
+	public static Entry<Integer, List<String>> nthHighestSalary4(int index, Map<String, Integer> map) {
+		
+		 return map.entrySet().stream().collect(Collectors.groupingBy(Map.Entry::getValue, 
+				Collectors.mapping(Map.Entry::getKey, Collectors.toList())))
+				.entrySet()
+				.stream().sorted(Collections.reverseOrder(Map.Entry.comparingByKey()))
+				.collect(Collectors.toList()).get(index);
+		
+		  
+	}
 
 	public static Map<String, Integer> getMap() {
 		Map<String, Integer> map = new HashMap<String, Integer>();
@@ -49,6 +63,17 @@ public class NthHighestSalaryExample {
 		map.put("Anil", 1200);
 		map.put("Dinesh", 1400);
 		map.put("Rajesh", 1700);
+
+		return map;
+	}
+	
+	public static Map<String, Integer> getMap2() {
+		Map<String, Integer> map = new HashMap<String, Integer>();
+		map.put("Pankaj", 1500);
+		map.put("Rakesh", 1500);
+		map.put("Anil", 1200);
+		map.put("Dinesh", 1200);
+		map.put("Rajesh", 1600);
 
 		return map;
 	}
