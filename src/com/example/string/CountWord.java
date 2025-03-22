6a6899e30d1dc08dcd0a68eff2247044379fdd68
kaushal kumar <kaushal.k.0014@gmail.com>
2025-03-22 22:33:59 +0530

    fixing issue

--- src/com/example/string/CountWord.java
+++ src/com/example/string/CountWord.java
@@ -1,20 +1,28 @@
 package com.example.string;
 
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.Map;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 
 public class CountWord {
-	
+
 	public static void main(String[] args) {
-		  String text = "Java is great and Java is powerful and Java is fun";
-		  
-		  Map<String, Long> result=Arrays.asList(text)
-		  .stream()
-		  .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
-		  
-		  System.out.println("Result :");
-		  System.out.println(result);
+		String text = "Java is great and Java is powerful and Java is fun";
+		useingMap(text.split("\s"));
+		Map<String, Long> result = Arrays.asList(text.split("\s")).stream()
+				.collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
+
+		System.out.println("Result using java 8:");
+		System.out.println(result);
+	}
+
+	private static void useingMap(String[] arr) {
+		Map<String, Integer> map = new HashMap<String, Integer>();
+		for (String str : arr) {
+			map.put(str, map.getOrDefault(str, 0) + 1);
+		}
+		System.out.println(map);
 	}
 }
