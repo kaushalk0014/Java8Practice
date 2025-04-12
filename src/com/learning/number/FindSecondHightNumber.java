46f96ff27b98b85f9a30ba29d285e091d1100c6d
kaushal kumar <kaushal.k.0014@gmail.com>
2025-04-12 22:05:34 +0530

    adding find second higest number

--- src/com/learning/number/FindSecondHightNumber.java
+++ src/com/learning/number/FindSecondHightNumber.java
@@ -1,5 +1,25 @@
 package com.learning.number;
 
-public class FindSecondHightNumber {
+import java.util.Arrays;
+import java.util.Comparator;
 
+public class FindSecondHightNumber {
+	/*
+	 * Find second highest number from given array
+	 * 
+	 */
+	
+	
+	public static void main(String[] args) {
+		int arr[]= {45,12,65,89, 89,17,45,12};
+		
+		Integer list=Arrays.stream(arr).boxed()
+		.sorted(Comparator.reverseOrder()).distinct()
+		.skip(1)
+		.findFirst().get();
+		 
+		
+		System.out.println(list);
+		System.out.println("------------------------------");
+	}
 }
