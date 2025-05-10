401a10d2be7d83f680367416db46b8d7a8319606
kaushal kumar <kaushal.k.0014@gmail.com>
2025-05-10 13:41:08 +0530

    adding find anagrams method

---
+++ src/com/learning/string/FindAnagrams.java
@@ -0,0 +1,35 @@
+package com.learning.string;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class FindAnagrams {
+
+	public static void main(String[] args) {
+
+		System.out.println(findAnagram("listen", "silent")); // true
+		System.out.println(findAnagram("hello", "world")); // false
+	}
+
+	private static boolean findAnagram(String str1, String str2) {
+		Map<Character, Integer> map = new HashMap<Character, Integer>();
+
+		for (char ch : str1.toCharArray()) {
+			map.put(ch, map.getOrDefault(ch, 0) + 1);
+		}
+
+		for (char ch : str2.toCharArray()) {
+			if (map.containsKey(ch)) {
+				if (map.get(ch) == 1) {
+					map.remove(ch);
+				} else {
+					map.put(ch, map.get(ch) + 1);
+				}
+			} else {
+				return false;
+			}
+		}
+
+		return map.isEmpty();
+	}
+}
