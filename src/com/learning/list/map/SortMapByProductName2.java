cfd43f78b11fabff45f6dbed7c402c7bd77ea37f
kaushal kumar <kaushal.k.0014@gmail.com>
2025-06-17 21:37:06 +0530

    map sort

---
+++ src/com/learning/list/map/SortMapByProductName2.java
@@ -0,0 +1,24 @@
+package com.learning.list.map;
+
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class SortMapByProductName2 {
+	public static void main(String[] args) {
+		Map<Integer, Product> map = new HashMap<>();
+		map.put(101, new Product(101, "Laptop", 80000d));
+		map.put(102, new Product(102, "Phone", 50000d));
+		map.put(103, new Product(103, "Book", 3000d));
+		
+		Map<Integer, Product> sortedMap =map.entrySet().stream()
+		.sorted(Map.Entry.comparingByValue(Comparator.comparing(Product::getName)))
+		.collect(Collectors.toMap(Map.Entry::getKey,Map.Entry::getValue,
+				(e1, e2)->e1,
+				LinkedHashMap::new));
+		
+		System.out.println(sortedMap);
+	}
+}
