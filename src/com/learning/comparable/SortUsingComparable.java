f3370c4eb72fbd623df5f9ec48753fa6493c5d74
kaushal kumar <kaushal.k.0014@gmail.com>
2025-07-04 22:00:31 +0530

    adding comparable sort

--- src/com/learning/comparable/SortUsingComparable.java
+++ src/com/learning/comparable/SortUsingComparable.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
+
 public class SortUsingComparable  {
 
 	 
@@ -20,6 +21,27 @@ public class SortUsingComparable  {
 		 System.out.println("\n After sorting-------------");
 		 Collections.sort(list);
 		 System.out.println(list);
+		 System.out.println("---------------------------------");
+		 //SortUsingComparable.sordData();
 	}
 	
+	public void sordData() {
+
+		//we can't sort data using Comparable because comparable using this key word
+		List<Customer> list= new ArrayList<Customer>();
+		list.add(new Customer(101, "Kaushal", 25));
+		list.add(new Customer(105, "Pankaj", 32));
+		list.add(new Customer(108, "Rahul", 28));
+		list.add(new Customer(104, "Ayush", 35));
+		
+		Comparable<Customer> comparable =  new Comparable<Customer>() {
+			
+			@Override
+			public int compareTo(Customer o) {
+				 //Integer.compare(this, 0);
+				return 0;
+			}
+		};
+		
+	}
 }
